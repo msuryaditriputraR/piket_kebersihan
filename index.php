dce9fc121243b292c92e0573942219a73bb838cf
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2025-04-15 21:30:41 +0800

    heading

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PIKET</title>
+
+  <!--- ==================== CSS ===================== -->
+  <link rel="stylesheet" href="src/style.css">
+</head>
+
+<body>
+  <h1>Jadwal Piket Kebersihan <br> <span id="kelas">XI TKJ 2</span></h1>
+
+  <table>
+
+  </table>
+
+  <!--- ==================== JAVASCRIPT ===================== -->
+  <script src="src/main.js"></script>
+</body>
+
+</html>
